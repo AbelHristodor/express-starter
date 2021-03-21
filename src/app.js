1460fa9f12199b6103e5f9aee2ee6d1c1b0f9168
AbelHristodor <habelor@gmail.com>
2021-03-21 12:39:05 +0100

    general code refactoring, added cors protection

--- src/app.js
+++ src/app.js
@@ -1,29 +1,53 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
-
-require('dotenv').config();
+const cookieParser = require('cookie-parser');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+const app = express();
 
 const middlewares = require('./middlewares.js');
 const api = require('./api');
 
-const app = express();
+// Allows only requests from a list of domains
+var whitelist = ['http://localhost:4000', 'http://localhost:3000']; //white list consumers
+var corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(null, false);
+        }
+    },
+    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    credentials: true, //Credentials are cookies, authorization headers or TLS client certificates.
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept']
+};
 
+// Standard middlewares
 app.use(morgan(process.env.LOGGER_FORMAT));
 app.use(helmet());
-app.use(cors());
+app.use(cors(corsOptions));
+app.use(cookieParser());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({
-        message: "Hello World"
-    });
-});
-
+// API routes
 app.use('/api', api);
 
+// Route not found middleware
 app.use(middlewares.notFound);
+
+// Error handlers after all other middlewares
 app.use(middlewares.errorHandler);
 
-module.exports = app;
+// Starting Server
+const port = process.env.EXPRESS_PORT || 3000
+const host = process.env.EXPRESS_HOST || 'localhost'
+
+app.listen(port, () => {
+    /* eslint-disable no-console */
+    console.log(`Listening: http://${host}:${port}`)
+    /* eslint-enable no-console */
+})
--- src/middlewares.js
+++ src/middlewares.js
@@ -1,20 +1,31 @@
-const notFound = (req, res, next) => {
-    res.status(404);
-    const error = new Error('404 - Not Found');
-    next(error);
-}
-/* eslint-disable no-unused-vars */
-const errorHandler = (err, req, res, next) => {
-    /* eslint-enable no-unused-vars */
-    const status = res.status !== 200 ? res.statusCode : 500;
-    res.status(status);
+/* Custom middlewares */
 
-    res.json({
-        message: err.message,
-    });
-}
+const notFound = (req, res, next) => {
+    // Create error and add some info to it
+    let error = new Error('Route not found');
+    error.statusCode = 404;
+
+    // Sending error to the errorHandler middleware
+    next(error);
+};
+
+const errorHandler = (error, req, res, next) => {
+    // Generic Error handler
+
+    // If status code of error not found then set it to a generic 500 internal server error
+    if (!error.statusCode) error.statusCode = 500;
+
+    // Send error
+    res.status(error.statusCode).send({
+        error: {
+            status: error.statusCode,
+            message: error.message || 'Internal Server Error',
+            stack: error.stack
+        }
+    })
+};
 
 module.exports = {
-    notFound,
-    errorHandler
-};
+    errorHandler,
+    notFound
+}
